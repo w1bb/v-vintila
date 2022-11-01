21e27fb4ea3ad534efe90eb06d207ab233a521ae
Valentin-Ioan Vintila <vintilavalentinioan@gmail.com>
2022-11-01 14:18:23 -0700

    kind of removed left navbar

---
+++ src/index.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {
+    indexNavbarOnScroll();
+}
+
+function indexNavbarOnScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#index-nav-top").addClass("scrolled");
+        $("#index-nav-left").addClass("scrolled");
+    } else {
+        $("#index-nav-top").removeClass("scrolled");
+        $("#index-nav-left").removeClass("scrolled");
+    }
+}
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+    
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 400, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+  });
