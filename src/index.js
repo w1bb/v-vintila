e1515f27ae541e2b74e3b8b48cc5d38aa3a04fec
Valentin-Ioan Vintila <vintilavalentinioan@gmail.com>
2022-11-05 05:42:40 -0700

    mostly text for N-AI Spam and the numerical methods book

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ function article_content(content_of) {
                     <li>A faster evaluation algorithm.</li>\
                 </ul>\
                 <p>For more info, please check out the <a href=\"#\">code repository</a>. The code is made available under the <a href=\"#\">? license</a>.</p>\
-                <p>I value keeping my code open-source. However, it's disheartening whenever I find that someone has copied my code without giving me proper credit. All I ask of you is to not claim my effort as your own.</p>\
+                <p>I value keeping my code open-source. However, it's disheartening whenever I find that someone has copied my work without giving me proper credit. All I ask of you is to not claim my effort as your own.</p>\
             </div>"
         );
     } else if(content_of == "proj-WI-CRYPT") {
@@ -119,7 +119,59 @@ function article_content(content_of) {
                     <li>Wikipedia also talks about <a href=\"https://en.wikipedia.org/wiki/Advanced_Encryption_Standard\">AES</a> and links to various important pieces of information that were used in the algorithm.</li>\
                 </ul>\
                 <p>For more info, please check out the <a href=\"#\">code repository</a>. The code is made available under the <a href=\"#\">? license</a>.</p>\
-                <p>I value keeping my code open-source. However, it's disheartening whenever I find that someone has copied my code without giving me proper credit. All I ask of you is to not claim my effort as your own.</p>\
+                <p>I value keeping my code open-source. However, it's disheartening whenever I find that someone has copied my work without giving me proper credit. All I ask of you is to not claim my effort as your own.</p>\
+            </div>"
+        );
+    } else if(content_of == "proj-N-AI-SPAM") {
+        $("#article-main-info").html(
+            "<div class=\"date\">January 2022</div>\
+            <div class=\"title\">N-AI Spam</div>\
+            <div class=\"text\">\
+                <p>2022's Baliza challenge (by Bitdefender) was all about creating a <b>C</b> (or Python) program that could detect 2000's spam emails based on their title and content. My algorithm won <b>1st place</b>, having an <a href=\"https://en.wikipedia.org/wiki/F-score\">F-score</a> of F1=95.63%.</p>\
+                <p>The programs were NOT allowed to use any kind of AI, so they were limited by the quality of the chosen heuristics. Moreover, the participants were given 150 emails to test their algorithms on, but these were different than the emails used in the final evaluation. We were not given any hints or guides, so every implementaion detail was up to us.</p>\
+                <p>So, based on what I knew, I came up with a scoring system that would change once a certain value would be passed:</p>\
+                <ul>\
+                    <li>score &lt; 35: the email is definitely HAM (<b>DHAM</b>), a.k.a. non-spam;</li>\
+                    <li>35 &leq; score &lt; 42: the email is probably HAM (<b>PHAM</b>);</li>\
+                    <li>42 &leq; score &lt; 50: the email is a suspicious HAM (<b>SHAM</b>);</li>\
+                    <li>50 &leq; score: the email is <b>SPAM</b>;</li>\
+                </ul>\
+                <p>Some of the heuristics used include:</p>\
+                <ul>\
+                    <li><b>Punctuation</b> could indicate a SPAM email - for example, dollar signs could indicate money transfers wanted by potential malicious users, while exclamation marks could indicate urgency. A score is determined based on the number of characters relative to the number of words counted in the email;</li>\
+                    <li><b>Uppercase</b> letters are usually used in excess or not at all when it comes to SPAM email. <a href=\"https://www.desmos.com/calculator/lw4qgluitr\">Here</a> is the formula I found best suited for computing this score. In the end, everything is converted relative to the toal count of non-space characters;</li>\
+                    <li><b>Consonants</b> don't appear naturally in groups of 4 or more, so this clue could raise a red flag. Obviously, links should be ignored;</li>\
+                    <li><b>Keywords</b> that trigger a SPAM are very important - they include words such as 'money', 'purchase', 'deposit', 'diamond', 'risk', 'bank' etc. However, there are some words that could lower this score, including 'forwarded', 'newsletter' and 'yahoo' (it was big back then). If the same word occures multiple times, say n times in the same email, it should be counted only about sqrt(n) times (my formula is a little bit more complicated, but for small values this works too);</li>\
+                    <li><b>Known spam email addresses</b> should also be taken into account.</li>\
+                </ul>\
+                <p>There are some other metrics that were used, but all in all, this is the algorithm. In the end, it also checks if there are duplicate emails inputed, since these are most likely SPAM. If an email is considered a PHAM or an SHAM, its score will be reevaluated to make sure it isn't actually a SPAM.</p>\
+                <p>The algorithm was written in <b>C</b> and it was <b>tested</b> using some custom made tests as well. The code is <b>very well documented</b>.</p>\
+                <p>Note: the original challenge also contained some other task, but that one was trivial and it was only used to filter those who wanted to win an award from those who weren't all that interested.</p>\
+                <p>For more info, please check out the <a href=\"#\">code repository</a>. The code is made available under the <a href=\"#\">? license</a>.</p>\
+                <p>I value keeping my code open-source. However, it's disheartening whenever I find that someone has copied my work without giving me proper credit. All I ask of you is to not claim my effort as your own.</p>\
+            </div>"
+        );
+    } else if(content_of == "proj-MN") {
+        $("#article-main-info").html(
+            "<div class=\"date\">Work in progress</div>\
+            <div class=\"title\">A numerical methods book</div>\
+            <div class=\"text\">\
+                <p>I had always found numerical methods fascinating. However, whenever I had to study something in this field, I had always felt lost, since all resources I had found are way too vague, incomplete or completly wrong. And so, I started writing my own numerical methods book.</p>\
+                <p>The project is currently written in <b>LaTeX</b> and will be soon released under an <b>open-source</b> license. Some of the covered materials include:</p>\
+                <ul>\
+                    <li><b>LU decomposition</b>: Doolittle, Crout, Cholesky, linear system of equations, LDU decomposition and PLU explanation;</li>\
+                    <li><b>QR decomposition</b>: Gram-Schmidt and its modified, more stable version, Householder and Givens;</li>\
+                    <li><b>Gaussian Elimination</b>: and its many variations, including partial pivoting, partial pivoting with a scaled pivot, total pivoting, Thomas algorithm and Gauss-Jordan Elimination;</li>\
+                    <li><b>Iterative methods</b> for solving LSE: Jacobi, Gauss-Seidel, successive over-relaxation method and gradient descent;</li>\
+                    <li><b>Non-linear equations</b>: successive approximations method, bisection method, tangent method, secant method and systems of non-linear equations;</li>\
+                    <li><b>Interpolation methods</b>: polynomial methods (Vandermonde, Lagrange, Neville, Newton), spline methods, Bézier curves and De Casteljau's algorithm</li>\
+                    <li><b>Approximation methods</b>: least squares methods, methods based on orthogonal polynomials and rational functions methods;</li>\
+                    <li><b>Numerical derivatives</b>: simple formula, 3-points method, Richardson extrapolation and higher-order derivatives methods;</li>\
+                    <li><b>Numerical integration</b>: quadrature methods (rectangle method, trapezoidal method and Simpson methods), Newton-Cotes methods, Romberg methods, adaptive quadratures and Gaussian quadratures;</li>\
+                    <li><b>Ordinal Differential Equations</b>: Euler's method, explicit Runge-Kutta methods and multistep methods;</li>\
+                </ul>\
+                <p>Bonus materials include the <b>PageRank</b> algorithm and <b>FFT</b>. All the code presented in the book is written in <b>Octave</b>, but I plan to convert it in <b>Julia</b> in the next editions.</p>\
+                <p>However, the book is only 80% ready and it written in Romanian, so it will not be released just yet. More info will come soon.</p>\
             </div>"
         );
     } else {
